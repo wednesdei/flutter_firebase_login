f29435f23197f7943efa5a106490ca6b7c7fd324
wednesdei <quartzoakash@anyport.org>
2022-05-29 16:20:39 -0600

    final working now

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_firebase_login/app/app.dart';
+import 'firebase_options.dart';
 
 Future<void> main() {
   return BlocOverrides.runZoned(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
-      await Firebase.initializeApp();
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
       final authenticationRepository = AuthenticationRepository();
       await authenticationRepository.user.first;
       runApp(App(authenticationRepository: authenticationRepository));
